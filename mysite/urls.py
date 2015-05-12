a456486d2dac87cc410303e041d7713f26b70d20
mayihu <mayihu@sina.com>
2015-05-12 19:15:24 +0800

    mysite init
    Signed-off-by: mayihu <mayihu@sina.com>

---
+++ mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+   # url(r'^$', 'mysite.views.home', name='home'),
+     #url(r'^blog/', include('blog.urls')),
+#url(r'', include('blog.urls')),
+     url(r'^admin/', include(admin.site.urls)),
+	 url(r'^$', 'article.views.home'),
+	 url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
+	 url(r'^test/$', 'article.views.test',name='test'),
+	#url(r'^hello/', include(hello.urls)),
+)
